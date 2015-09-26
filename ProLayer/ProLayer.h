1427c161a2307e8f5565455f36a5e758c2a0af33
Ivan Brazhnikov <samsungpc239@gmail.com>
2015-09-26 18:01:23 +0300

    Initial Commit

---
+++ ProLayer/ProLayer.h
@@ -0,0 +1,19 @@
+//
+//  ProLayer.h
+//  ProLayer
+//
+//  Created by Ivan Brazhnikov on 26/09/15.
+//  Copyright © 2015 Dantelab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ProLayer.
+FOUNDATION_EXPORT double ProLayerVersionNumber;
+
+//! Project version string for ProLayer.
+FOUNDATION_EXPORT const unsigned char ProLayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProLayer/PublicHeader.h>
+
+
